c16e5108777d33c9149dd6bd8bb3e24a16004d47
Akanksha Dubey <akankshadubey2558@gmail.com>
2024-04-30 14:53:01 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Component/Header/Header";
+import Navbar from "./Component/Navbar/Navbar";
+import Hero from "./Component/Hero/Hero";
+import Footer from "./Component/Footer/Footer";
+function App(){
+  return(
+    <>
+     <Header/>
+     <Navbar/>
+     <Hero/>
+     <Footer/>
+    </>
+  )
+}
+export default App;
